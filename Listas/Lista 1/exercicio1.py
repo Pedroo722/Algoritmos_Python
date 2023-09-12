43169af7032e456fc2e9d92de6675c3aea5afec0
Pedro Alexandre <132232273+Pedroo722@users.noreply.github.com>
2023-09-12 14:24:53 -0300

    Create exercicio1.py

---
+++ Listas/Lista 1/exercicio1.py
@@ -0,0 +1,6 @@
+n1 = int(input("Digite seu número: "))
+n2 = int(input("Digite outro número: "))
+
+resultado = n1 * n2
+
+print(f"O resultado da multiplicação de {n1} e {n2} é igual a {resultado}")
